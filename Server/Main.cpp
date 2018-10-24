e40ce266cac425d90d5929296b744be515487283
Simon Bourque <7109056+simon-bourque@users.noreply.github.com>
2018-10-24 13:06:17 -0400

    Add listen thread to server

--- Server/Main.cpp
+++ Server/Main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+#include "ThreadPool.h"
+
 int main() {
+	std::cout << "Initializing server..." << std::endl;
+
+	ThreadPool::init();
+
 	// Create listen thread
+	ThreadPool::get()->submit([](PTP_CALLBACK_INSTANCE instance, PVOID parameter, PTP_WORK work) {
+		UNREFERENCED_PARAMETER(work);
+
+		CallbackMayRunLong(instance);
+
+		bool listening = true;
+		while (listening) {
+			
+		}
+	}, nullptr);
+	
+	ThreadPool::destroy();
 	return 0;
 }
