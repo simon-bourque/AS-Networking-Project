d1bc0191c250ca6fa23a27e8c405b8a72f9b0213
Gabriel Bélanger <10086598+Gabbell@users.noreply.github.com>
2018-12-02 16:38:50 -0500

    Miracle cure

--- Client/Main.cpp
+++ Client/Main.cpp
@@ -4,6 +4,13 @@
 #include "WSA.h"
 
 int main() {
+	HANDLE inputHandle = GetStdHandle(STD_INPUT_HANDLE);
+	if (inputHandle != INVALID_HANDLE_VALUE) {
+		DWORD mode = 0;
+		GetConsoleMode(inputHandle, &mode);
+		SetConsoleMode(inputHandle, mode & (~ENABLE_QUICK_EDIT_MODE));
+	}
+
 	WSA::init();
 
 	std::string ip;
